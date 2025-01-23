b7a40373ddd6366f797cf53da3b8aa9297b63b70
Matt Phillips <mphillips8687@gmail.com>
2025-01-23 11:56:54 -0700

    Progress on Start/Stop During Lab

--- app/main.asm
+++ app/main.asm
@@ -26,6 +26,22 @@ init:
 
             bic.w   #LOCKLPM5,&PM5CTL0      ; Unlock I/O pins
 
+            ;-----------------------------------------
+            ;   Setting up I2C Pins
+            ;-----------------------------------------
+            ; Adding Pin 3.2 as output SCL
+            mov.w		#00h, P3SEL0
+            mov.w		#00h, P3SEL1
+            bis.b		#BIT2, &P3DIR			; Set P3.2 as an output. P3.2 is GPIO
+            bis.b		#BIT2, &P3OUT			; Set out to high
+
+            ; Adding Pin 3.0 as output SDA
+            mov.w		#00h, P3SEL0
+            mov.w		#00h, P3SEL1
+            bis.b		#BIT0, &P3DIR			; Set P3.0 as an output. P3.0 is GPIO
+            bis.b		#BIT0, &P3OUT			; Set out to high
+            ;-----------------------------------------
+
             ; Setup Clock
             bis.w	#TBCLR, &TB0CTL				; Clear timer & dividers
 			bis.w	#TBSSEL__ACLK, &TB0CTL		; Select ACLK as timer source
@@ -35,14 +51,53 @@ init:
 			bis.w	#TBIE, &TB0CTL			; Enable overflow interrupt
 			bic.w	#TBIFG, &TB0CTL			; Clear interrupt flag
             NOP
+
+            ; Disable Low Power Mode
+		    bic.w	#LOCKLPM5, &PM5CTL0
 			bis.w	#GIE, SR				; Enable maskable interrupts
             NOP
 
 main:
-
+            call    #i2c_start
             nop 
-            jmp main
+            jmp     main
             nop
+
+;------------------------------------------------------------------------------
+;           Subroutines
+;------------------------------------------------------------------------------
+
+i2c_start:
+
+            bic.b   #BIT0,&P3OUT
+            call    #i2c_start_delay
+            bic.b   #BIT2,&P3OUT
+            call    #i2c_start_delay
+
+            ret
+            nop
+;-------------- END i2c_start --------------
+
+i2c_stop:
+            bis.b   #BIT0,&P3OUT
+            jmp     main
+            nop
+;-------------- END i2c_start --------------
+
+i2c_start_delay:
+			bic.w	#GIE, SR				; Disable maskable interrupts
+
+            push    R4
+            mov.w   #60000, R4
+start_loop  dec     R4
+            jnz     start_loop
+            pop     R4
+
+			bis.w	#GIE, SR				; Enable maskable interrupts
+            ret
+            nop
+;-------------- END i2c_start --------------
+
 
 ;------------------------------------------------------------------------------
 ;           Interrupt Service Routines
@@ -51,6 +106,8 @@ main:
 ;TB0 Overflow
 ISR_TB0_Overflow                            ; Triggers every 1.0s 
             xor.b   #BIT6,&P6OUT
+            bis.b   #BIT0,&P3OUT
+            bis.b   #BIT2,&P3OUT
             bic.w   #TBIFG,&TB0CTL
             reti
 ;-------------- END ISR_TB0_Overflow --------------
