efe1f0f469ee1cf0e37a58cbe151ecf385238636
Matthew Vandeberg <matvandeberg@gmail.com>
2025-01-23 10:39:30 -0700

    LED blinker

--- app/main.asm
+++ app/main.asm
@@ -20,8 +20,23 @@ init:
             ; stop watchdog timer
             mov.w   #WDTPW+WDTHOLD,&WDTCTL
 
-            ; Disable low-power mode
-            bic.w   #LOCKLPM5,&PM5CTL0
+            ; Setup LED2
+            bic.b   #BIT6,&P6OUT            ; Clear P6.6 output
+            bis.b   #BIT6,&P6DIR            ; P6.6 output
+
+            bic.w   #LOCKLPM5,&PM5CTL0      ; Unlock I/O pins
+
+            ; Setup Clock
+            bis.w	#TBCLR, &TB0CTL				; Clear timer & dividers
+			bis.w	#TBSSEL__ACLK, &TB0CTL		; Select ACLK as timer source
+			bis.w	#MC__CONTINUOUS, &TB0CTL	; Choose continuous counting
+			bis.w	#CNTL_1, &TB0CTL        ; 2^12 
+			bis.w	#ID__8, &TB0CTL         ; Divide clock by 8
+			bis.w	#TBIE, &TB0CTL			; Enable overflow interrupt
+			bic.w	#TBIFG, &TB0CTL			; Clear interrupt flag
+            NOP
+			bis.w	#GIE, SR				; Enable maskable interrupts
+            NOP
 
 main:
 
@@ -29,10 +44,23 @@ main:
             jmp main
             nop
 
+;------------------------------------------------------------------------------
+;           Interrupt Service Routines
+;------------------------------------------------------------------------------
 
+;TB0 Overflow
+ISR_TB0_Overflow                            ; Triggers every 1.0s 
+            xor.b   #BIT6,&P6OUT
+            bic.w   #TBIFG,&TB0CTL
+            reti
+;-------------- END ISR_TB0_Overflow --------------
 
 ;------------------------------------------------------------------------------
 ;           Interrupt Vectors
 ;------------------------------------------------------------------------------
             .sect   RESET_VECTOR            ; MSP430 RESET Vector
             .short  RESET                   ;
+
+            .sect	".int42"                ; TB0 interrupt vector
+            .short	ISR_TB0_Overflow        ;
+            .end
